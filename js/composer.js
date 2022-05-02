4c8470b4e88d1844d6c6e94a313edf466bb71c7f
makosear <80222853+makosear@users.noreply.github.com>
2022-05-02 18:30:22 -0300

    beginning

---
+++ js/composer.js
@@ -0,0 +1,6 @@
+<select id="framework">
+    <option value="1">Makoto Naegi</option>
+    <option value="2">Kokichi Ouma</option>
+    <option value="3">Jataro Kemuri</option>
+    <option value="4">Keebo</option>
+</select>
