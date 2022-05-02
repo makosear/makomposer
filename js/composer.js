54d373a69cb6c48cf611dd818a3681a9fb776c1b
makosear <80222853+makosear@users.noreply.github.com>
2022-05-02 18:38:14 -0300

    Update composer.js

--- js/composer.js
+++ js/composer.js
@@ -1,6 +1,10 @@
-<select id="framework">
+<><select id="framework">
     <option value="1">Makoto Naegi</option>
     <option value="2">Kokichi Ouma</option>
     <option value="3">Jataro Kemuri</option>
     <option value="4">Keebo</option>
 </select>
+
+<button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+        Click me to display Date and Time.</button><p id="demo"></p></>
